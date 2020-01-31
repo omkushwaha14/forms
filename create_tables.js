0a082bcdc8ffd5e3d3a78e55086a4536a13b454a
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-02-01 03:09:18 +0530

    Update create_tables.js

--- create_tables.js
+++ create_tables.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2')
-const connection = mysql.createConnection({
-    host:'localhost',
-    database:'mytestdb',
-    user:'myusers',
-    password:'mypasss'
+ const connection = mysql.createConnection({
+    host: 'us-cdbr-iron-east-04.cleardb.net',
+    user: 'bf1e21f64c9bfb',
+    password: '6abaef19',
+    database: 'heroku_4964469205ceb69'
 })
 connection.query(`CREATE TABLE IF NOT EXISTS akashs(
 id INTEGER AUTO_INCREMENT PRIMARY KEY,
