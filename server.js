e620ae7bc60daad97dcf4d827531b3364523330e
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-02-02 04:06:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ res.redirect('/adds')
        res.send(err)
     })
 })
-app.listen(SERVER_PORT, () => {
-    console.log("Server started on http://localhost:3000/")
-})
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
