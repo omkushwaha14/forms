e83f481c83d5aaa2c63f41bcfd08dd47ae26f4e4
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-02-07 16:20:20 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 var connection = mysql.createPool({
-    host: 'us-cdbr-iron-east-04.cleardb.net',
-    user: 'bf1e21f64c9bfb',
-    password: '6abaef19',
-    database: 'heroku_4964469205ceb69'
+    host: 'ec2-34-192-30-15.compute-1.amazonaws.com',
+    user: 'pkcebsydrfznvn',
+    password: 'aa50a9bae407206f3d2e323ec1492fea282faeb7f1f5457ad7d533d4186f4b5f',
+    database: 'd2kq04nb5gp4c0'
 })
 
 function getAllPersons () {
