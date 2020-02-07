063b92bb3714f92fa620e1357a109ccf74d6fddc
Om Nath Kushwaha <58984004+omkushwaha14@users.noreply.github.com>
2020-02-07 18:21:09 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql')
 
-var connection = mysql.createPool({
+var connection = mysql.createConnection({
     host: 'ec2-34-192-30-15.compute-1.amazonaws.com',
     user: 'pkcebsydrfznvn',
     password: 'aa50a9bae407206f3d2e323ec1492fea282faeb7f1f5457ad7d533d4186f4b5f',
